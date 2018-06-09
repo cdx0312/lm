c5ee88d054a8874d3f679c7f159badcefb7d7250
cdx <cdxu0312@outlook.com>
2018-06-09 22:27:27 +0800

    添加报警事件的controller，service，dao

---
+++ src/main/java/com/hjh/service/AlertEventService.java
@@ -0,0 +1,18 @@
+package com.hjh.service;
+
+import com.hjh.domain.AlertEvent;
+
+import java.util.List;
+
+public interface AlertEventService {
+    /**
+     * 根据电梯编号，报警内容，报警状态和处理状态来查询报警记录
+     * @param liftId 电梯编号
+     * @param alertReason 报警原因
+     * @param state 报警状态
+     * @param process 处理状态
+     * @return 报警事件列表
+     */
+    List<AlertEvent> getAllAlertRecords(String liftId, Integer alertReason, Integer state, Integer process);
+
+}
