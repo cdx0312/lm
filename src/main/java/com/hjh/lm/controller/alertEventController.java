8f2b8000b581373240899b90b88dc352bf53150c
cdx <cdxu0312@outlook.com>
2018-06-10 22:22:44 +0800

    测试alertEventController方法

--- src/main/java/com/hjh/lm/controller/alertEventController.java
+++ src/main/java/com/hjh/lm/controller/alertEventController.java
@@ -5,18 +5,20 @@ import com.hjh.lm.service.AlertEventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/alertEvent")
 public class alertEventController {
 
     @Autowired
     private AlertEventService alertEventService;
 
-    @RequestMapping("/alertEvent/getRecords")
+    @RequestMapping(value = "/getRecords", method = RequestMethod.GET)
     @ResponseBody
     public List<AlertEvent> getAllAlertRecords(String liftId, Integer alertReason, Integer state, Integer process) {
         return alertEventService.getAllAlertRecords(liftId, alertReason, state, process);
