cbdd6e549bec056385c4396bf301b1cea8c32dd6
hjh <hjhui1994@163.com>
2018-06-14 16:08:32 +0800

    更新内容

--- src/main/java/com/hjh/lm/controller/FixRecordController.java
+++ src/main/java/com/hjh/lm/controller/FixRecordController.java
@@ -5,14 +5,15 @@ import com.hjh.lm.domain.FixRecord;
 import com.hjh.lm.service.FixRecordService;
 import com.hjh.lm.vo.LmResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -34,11 +35,14 @@ public class FixRecordController {
      * @param fixRecord 接收前端传送的维修数据的实体
      */
     @ResponseBody
-    @RequestMapping(value = "/repairRecord", method = RequestMethod.POST)
+    @RequestMapping(value = "/repairRecord/save" ,method=RequestMethod.POST)
     @CrossOrigin
     public LmResult saveFixRecord(FixRecord fixRecord){
-        LmResult lmResult = new LmResult("200", "add the fixRecord");
+        System.out.println("Entering saveFixRecord controller");
+        System.out.println(fixRecord.toString());
+        LmResult lmResult = new LmResult("200", "add the fixRecord", fixRecord);
         fixRecordService.save(fixRecord);
+        System.out.println("leaving saveFixRecord controller");
         return lmResult;
     }
 
@@ -47,7 +51,7 @@ public class FixRecordController {
      * @param liftId 电梯编号
      * @return 维修记录列表，如果liftID为空，则返回全部维修记录
      */
-    @RequestMapping(value = "/repairRecord", method = RequestMethod.GET)
+    @RequestMapping(value = "/repairRecord/get", method = RequestMethod.GET)
     @ResponseBody
     @CrossOrigin // 解决跨域
     public LmResult getFixRecordsByLiftId(@Param("liftid") String liftId){
